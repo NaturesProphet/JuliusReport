9fe6fe155571d4882c25e2b7ef30ca5afb90a79e
mgarcia <mateus.gigainfo@gmail.com>
2018-02-15 17:57:36 -0200

    Consulta de Rendimentos implementada

--- SCRIPT DO BANCO DE DADOS/QUERYs.sql
+++ SCRIPT DO BANCO DE DADOS/QUERYs.sql
@@ -1,5 +1,5 @@
 SELECT Rendimento.rowid, Rendimento.KmL, Rendimento.Trajeto, Rendimento.ArCond, Rendimento.Abastecimento
 FROM Rendimento JOIN  Abastecimento ON Rendimento.Abastecimento = Abastecimento.rowid
-WHERE Abastecimento.Veiculo = 1;
+WHERE Abastecimento.Veiculo = 1 ORDER BY Rendimento.Trajeto;
 
 
